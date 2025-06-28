07253f92f939b7bc49f17daf24990a9636b52534
Muhammad Zeeshan Zaheer <shan6818@gmail.com>
2025-06-28 18:16:40 +0500

    practice added more

---
+++ list.py
@@ -0,0 +1,18 @@
+marks=[90,89,98,989,89]
+print(type(marks))
+print(marks[0])
+student=["shan", 98.9, 87];
+print(student[1:3])
+#append method
+print(student.append(87))
+print(student)
+#sort method
+marks.sort() #sort using assending order
+print(marks)
+marks.sort(reverse = True)
+print(marks)
+marks.reverse
+print(marks)
+marks.insert(1, "shan")
+print(marks)
+
---
+++ multiple_of_7.py
@@ -0,0 +1,6 @@
+a =float(input("Enter the number :"))
+if(a % 7 ==0):
+  print(a, "is multiple of 7")
+else:
+  print(a, "is not multiple of 7");
+
